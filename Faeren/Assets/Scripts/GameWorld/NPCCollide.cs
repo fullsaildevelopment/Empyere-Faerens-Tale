ec0555e92ae83cd9fa2069692d2c868ca6682f52
ChanceMor <CCMorrison1@student.fullsail.edu>
2022-01-26 12:10:31 -0500

    Create NPCCollide.cs

---
+++ Faeren/Assets/Scripts/GameWorld/NPCCollide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCCollide : MonoBehaviour
+{
+    int collisionCount = 0;
+    private void OnCollisionEnter2D(Collision2D other) {
+        DialogTrigger dt = GetComponent<DialogTrigger>();
+        DialogueManager dm = FindObjectOfType<DialogueManager>();
+        //dt includes the character's dialog options
+        //Debug.Log(dt.dialog.name);
+        
+        if(collisionCount==(dt.dialog.sentences.Length+1) || name != dt.dialog.name)
+        {
+            collisionCount=0;
+            dm.EndDialogue();
+        }
+        
+        if(collisionCount==0){
+        dt.TriggerDialog();
+        }
+        else{
+            dm.DisplayNextSentence();
+
+        }
+        collisionCount++;
+
+    }
+}
