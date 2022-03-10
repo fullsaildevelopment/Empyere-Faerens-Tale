19414b8a50a34337bcf6bcaa16e5dedefd169d5e
VargentSomnium <bridgesmichael86@gmail.com>
2022-03-09 23:59:31 -0600

    Created First Equipment
    Created the games first equipment for the first 2 characters. There are 3 weapons and armors each for a total of 6. (Script carried over and revised from outside project)

--- Empyere-Faerens-tale/Assets/Scripts/Objects/dev/ItemCreation.cs
+++ Empyere-Faerens-tale/Assets/Scripts/Objects/dev/ItemCreation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ItemCreation : MonoBehaviour
 {
     List<Item> items = new List<Item>();
+    List<Equipment> equip = new List<Equipment>();
 
     private void Awake()
     {
@@ -86,6 +87,89 @@ public class ItemCreation : MonoBehaviour
         bomb.price = 20;
         items.Add(bomb);
         #endregion
+
+        #region Equipment Creation
+
+            #region Weapons
+
+        Equipment WoodenSword = new Equipment();
+        WoodenSword.Name = "Wooden Sword";
+        WoodenSword.boostType = "Attack";
+        WoodenSword.boost = 3;
+        equip.Add(WoodenSword);
+
+        Equipment CopperSword = new Equipment();
+        CopperSword.Name = "Copper Sword";
+        CopperSword.boostType = "Attack";
+        CopperSword.boost = 8;
+        equip.Add(CopperSword);
+
+        Equipment SteelSword = new Equipment();
+        SteelSword.Name = "Steel Sword";
+        SteelSword.boostType = "Attack";
+        SteelSword.boost = 14;
+        equip.Add(SteelSword);
+
+        Equipment WoodBuckler = new Equipment();
+        WoodBuckler.Name = "Wood Buckler";
+        WoodBuckler.boostType = "Attack";
+        WoodBuckler.boost = 5;
+        equip.Add(WoodBuckler);
+
+        Equipment StuddedShield = new Equipment();
+        StuddedShield.Name = "Studded Shield";
+        StuddedShield.boostType = "Attack";
+        WoodBuckler.boost = 16;
+        equip.Add(StuddedShield);
+
+        Equipment TowerShield = new Equipment();
+        TowerShield.Name = "Tower Shield";
+        TowerShield.boostType = "Defense";
+        TowerShield.boost = 16;
+        equip.Add(TowerShield);
+
+        #endregion
+            #region Armors
+
+        Equipment LeatherVest = new Equipment();
+        LeatherVest.Name = "Leather Vest";
+        LeatherVest.boostType = "Defense";
+        LeatherVest.boost = 4;
+        equip.Add(LeatherVest);
+
+        Equipment ChainmailVest = new Equipment();
+        ChainmailVest.Name = "Chainmail Vest";
+        ChainmailVest.boostType = "Defense";
+        ChainmailVest.boost = 9;
+        equip.Add(ChainmailVest);
+
+        Equipment SteelPlate = new Equipment();
+        SteelPlate.Name = "Steel Plate";
+        SteelPlate.boostType = "Defense";
+        SteelPlate.boost = 16;
+        equip.Add(SteelPlate);
+
+        Equipment StiffLeatherGauntlents = new Equipment();
+        StiffLeatherGauntlents.Name = "Stiff Leather Gauntlents";
+        StiffLeatherGauntlents.boostType = "Defense";
+        StiffLeatherGauntlents.boost = 6;
+        equip.Add(StiffLeatherGauntlents);
+
+        Equipment ReinforcedGauntlets = new Equipment();
+        ReinforcedGauntlets.Name = "Reinforced Gauntlets";
+        ReinforcedGauntlets.boostType = "Defense";
+        ReinforcedGauntlets.boost = 13;
+        equip.Add(ReinforcedGauntlets);
+
+        Equipment SteelGauntlets = new Equipment();
+        SteelGauntlets.Name = "Steel Gauntlets";
+        SteelGauntlets.boostType = "Defense";
+        SteelGauntlets.boost = 18;
+        equip.Add(SteelGauntlets);
+
+            #endregion
+
+        #endregion
     }
 
     public void createitems()
@@ -94,6 +178,14 @@ public class ItemCreation : MonoBehaviour
         {
             Serializer sz = new Serializer();
             sz.SerializeItem(item);
+        }
+    }
+    public void createequip()
+    {
+        foreach(Equipment equipment in equip)
+        {
+            Serializer sz = new Serializer();
+            sz.SerializeEquipment(equipment);
         }
     }
 
