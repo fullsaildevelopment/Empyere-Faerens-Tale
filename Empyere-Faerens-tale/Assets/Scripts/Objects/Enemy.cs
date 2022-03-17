4e24694df42cd7e07a8a9d1fab2fca651b5425ea
VargentSomnium <bridgesmichael86@gmail.com>
2022-03-17 01:04:32 -0500

    Tank and Healer Logic
    Wrote basic tank and healer logic.
    
    To be expanded upon next week.

--- Empyere-Faerens-tale/Assets/Scripts/Objects/Enemy.cs
+++ Empyere-Faerens-tale/Assets/Scripts/Objects/Enemy.cs
@@ -47,11 +47,12 @@ public class Enemy : Character
 
     #endregion
 
+    //Basic Logic for enemies. Need to expand. March 16 22
     #region Logic
 
     public Skill attackerLogic()
     {
-        if(CurrentHealth > MaxHealth/2)
+        if (CurrentHealth > MaxHealth / 2)
         {
             System.Random random = new System.Random();
             int i = random.Next(100);
@@ -62,13 +63,13 @@ public class Enemy : Character
             List<Skill> options = new List<Skill>();
             foreach (Skill skill in skills)
             {
-                if(skill.type == SkillType.Damage)
+                if (skill.type == SkillType.Damage)
                 {
                     options.Add(skill);
                 }
             }
-            
-            int choice = random.Next(options.Count+1);
+
+            int choice = random.Next(options.Count + 1);
             return options[choice];
         }
         else
@@ -77,9 +78,9 @@ public class Enemy : Character
             List<Skill> options = new List<Skill>();
             foreach (Skill skill in skills)
             {
-                if(skill.type == SkillType.Heal && skill.target == SkillTarget.Self)
+                if (skill.type == SkillType.Heal && skill.target == SkillTarget.Self)
                 {
-                    if(random.Next(100) > 75)
+                    if (random.Next(100) > 75)
                     {
                         return skill;
                     }
@@ -104,6 +105,137 @@ public class Enemy : Character
         }
     }
 
-    #endregion
+    //Needs to be expanded
+    public Skill tankLogic()
+    {
+        if (CurrentHealth > MaxHealth / 3)
+        {
+            System.Random random = new System.Random();
+            int i = random.Next(100);
+            if (i > 35)
+            {
+                return attack;
+            }
+            List<Skill> options = new List<Skill>();
+            foreach (Skill skill in skills)
+            {
+                if (skill.type == SkillType.Support || skill.type == SkillType.Damage)
+                {
+                    options.Add(skill);
+                }
+            }
 
+            int choice = random.Next(options.Count + 1);
+            return options[choice];
+        }
+        else
+        {
+            System.Random random = new System.Random();
+            List<Skill> options = new List<Skill>();
+            foreach (Skill skill in skills)
+            {
+                if (skill.type == SkillType.Heal && skill.target == SkillTarget.Self)
+                {
+                    if (random.Next(100) > 75)
+                    {
+                        return skill;
+                    }
+                }
+            }
+            int i = random.Next(100);
+            if (i > 45)
+            {
+                return attack;
+            }
+            foreach (Skill skill in skills)
+            {
+                if (skill.type == SkillType.Support)
+                {
+                    options.Add(skill);
+                }
+            }
+
+            int choice = random.Next(options.Count + 1);
+            return options[choice];
+        }
+
+    }
+
+    //Need to refine
+    public Skill healerLogic(List<Character> characters)
+    {
+        if (CurrentHealth > MaxHealth / 3)
+        {
+            System.Random random = new System.Random();
+            int i = random.Next(100);
+            if (i > 90)
+            {
+                return attack;
+            }
+            List<Skill> options = new List<Skill>();
+            foreach (Skill skill in skills)
+            {
+                if (skill.type == SkillType.Heal)
+                {
+                    options.Add(skill);
+                }
+            }
+            int partyMax = 0;
+            int partyCurrent = 0;
+            foreach (Character character in characters)
+            {
+                partyMax += character.MaxHealth;
+                partyCurrent += character.CurrentHealth;
+            }
+            if (random.Next(partyMax) < partyCurrent)
+            {
+                return options[random.Next(options.Count + 1)];
+            }
+        }
+        else
+        {
+            System.Random random = new System.Random();
+            List<Skill> options = new List<Skill>();
+            foreach (Skill skill in skills)
+            {
+                if (skill.type == SkillType.Heal && skill.target == SkillTarget.Self)
+                {
+                    if (random.Next(100) > 80)
+                    {
+                        return skill;
+                    }
+                }
+            }
+            int i = random.Next(100);
+            if (i > 95)
+            {
+                return attack;
+            }
+            foreach (Skill skill in skills)
+            {
+                if (skill.type == SkillType.Heal && skill.target == SkillTarget.Party)
+                {
+                    options.Add(skill);
+                }
+            }
+            if (random.Next(100) > 50)
+            {
+                return options[random.Next(options.Count + 1)];
+            }
+
+            return skills[random.Next(skills.Count + 1)];
+
+        }
+        System.Random rand = new System.Random();
+        return skills[rand.Next(skills.Count + 1)];
+    }
+
+    //Need to refine and expand upon
+    public Skill supportLogic()
+    {
+
+    }
+
+
+    #endregion
 }
