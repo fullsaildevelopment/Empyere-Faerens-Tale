6c9f5255b46414546e168abefa56d28c32a565a8
VargentSomnium <bridgesmichael86@gmail.com>
2022-03-24 07:33:06 -0500

    ELH General Damage Targeting
    ELH now handles targeting in general for damage type skills.
    
    Returns an int and enum. The int is the position in party and the enum is which party. Ally or Opposition.

--- Empyere-Faerens-tale/Assets/Scripts/Objects/EnemyLogicHandler.cs
+++ Empyere-Faerens-tale/Assets/Scripts/Objects/EnemyLogicHandler.cs
@@ -4,11 +4,62 @@ using UnityEngine;
 
 public class EnemyLogicHandler
 {
+    Party ally = new Party();
+    Party opposition = new Party();
+    Character current = new Character();
+    Skill selectedSkill = new Skill();
    
     public void StartELH(Skill skill, Character character)
     {
-        Party ally = character.GetParty();
-        Party opposition = ally.Opposition;
+        ally = character.GetParty();
+        opposition = ally.Opposition;
+        PartyDesignation targetParty;
+        int targetPosition;
+        ELHSkillDesignation(skill.type, out targetPosition, out targetParty);
+    }
+    public void ELHSkillDesignation(SkillType st, out int position, out PartyDesignation partyDesignation)
+    {
+        switch (st)
+        {
+            case SkillType.Damage:
+                position = ELHDamage();
+                partyDesignation = PartyDesignation.Opposition;
+                break;
+            case SkillType.Heal:
+
+                break;
+            case SkillType.Support:
+                break;
+            default:
+                break;
+        }
+    }
+    //returns 1-4 4 being entire party
+    public int ELHDamage()
+    {
+        int selection = 0;
+        int final = 0;
+        if (selectedSkill.target == SkillTarget.Party)
+        {
+            return 4;
+        }
+        foreach (Character character in opposition.active_characters)
+        {
+            if (selection == 0)
+            {
+                selection = 1;
+            }
+            else if (opposition.active_characters[selection].CurrentHealth < character.MaxHealth)
+            {
+                selection++;
+                final = selection;
+            }
+            else
+            {
+                selection++;
+            }
+        }
+        return final;
     }
 
 }
