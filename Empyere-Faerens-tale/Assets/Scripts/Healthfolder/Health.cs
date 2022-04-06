1f1ecb3a59c9924323a816ba5e5a3b2538d8d445
Char <63970461+Chardelyce@users.noreply.github.com>
2022-04-05 21:21:42 -0500

    BIG CHANGELIST
    -RE DID ART FOR THE CAPITAL, PLAINS , AND CASTLE LEVELS
    -SIGNS IN CASTLE
    -ADDED SNOW
    -ADDED NEW PLAYER , AND PARTY SPRITES
    -ADDED OVERWORLD ATTACK (BETA)
    -ADDED PARRALAX SCROLLING

---
+++ Empyere-Faerens-tale/Assets/Scripts/Healthfolder/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        if(currentHealth> 0)
+        {
+            //if the player is dead or alive
+        }
+        else
+        {
+            //player died :(
+        }
+    }
+   /* private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            TakeDamage(1);
+        }
+    }
+   */
+}
