aaf5e3a10dab811ce3202c9478c6b5f0a7b3f4a2
VargentSomnium <bridgesmichael86@gmail.com>
2022-04-06 18:06:05 -0500

    Inventory UI
    Created UI scene and attempted to populate.
    
    Got stuck in a brick wall trying to populate it.

---
+++ Empyere-Faerens-tale/Assets/Scenes/Inventory/Inventory Assets/popInvDebug.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class popInvDebug : MonoBehaviour
+{
+    public void popInv()
+    {
+        Inventory inv = GameObject.Find("GameController").GetComponent<GameUser>().user.inventory;
+        #region adding items
+        Serializer sz = new Serializer();
+        Equipment sword = new Equipment();
+        sword.Name = "Copper Sword";
+        sword.ObjectType = Identiy.Equipment;
+        Item item = new Item();
+        item.Name = "Bomb";
+        item.ObjectType = Identiy.Item;
+        inv.ObjectList = new List<BaseObject>();
+        inv.ObjectList.Add(item);
+        inv.ObjectList.Add(item);
+        inv.ObjectList.Add(sword);
+        SceneManager.LoadScene(11, LoadSceneMode.Single);
+        #endregion
+        
+    }
+}
