64cb950099b758588e0df4c98f634d8f3ff379d3
ChanceMor <CCMorrison1@student.fullsail.edu>
2022-04-25 12:05:04 -0400

    pathblockage system
    developed a system allowing us to block certain areas, till the player met certain requirements

---
+++ Empyere-Faerens-tale/Assets/GlobalVars.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool pathunlocked;
+    void Start()
+    {
+        pathunlocked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
