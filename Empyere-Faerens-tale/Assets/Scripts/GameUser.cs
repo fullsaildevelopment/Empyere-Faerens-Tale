50c7ccaab9c48939e379e84316a51b2d1c0a7065
VargentSomnium <bridgesmichael86@gmail.com>
2022-05-11 17:15:37 -0500

    Combat Fix
    Fixed combat. The splice caused some issues those are now solved

--- Empyere-Faerens-tale/Assets/Scripts/GameUser.cs
+++ Empyere-Faerens-tale/Assets/Scripts/GameUser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameUser
+public class GameUser : MonoBehaviour
 {
     public User user = new User("User");
 
