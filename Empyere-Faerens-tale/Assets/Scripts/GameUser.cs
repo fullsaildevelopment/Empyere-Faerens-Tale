bbf85c44aff3b33c4e0e25055d6a30b0fe2c3f4a
VargentSomnium <bridgesmichael86@gmail.com>
2022-05-11 16:55:24 -0500

    Added missing files + fixing combat from splice

--- Empyere-Faerens-tale/Assets/Scripts/GameUser.cs
+++ Empyere-Faerens-tale/Assets/Scripts/GameUser.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameUser : MonoBehaviour
+public class GameUser
 {
     public User user = new User("User");
-    void Start()
-    {
-    }
 
 }
