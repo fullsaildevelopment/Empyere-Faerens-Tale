982ec4e4eb33cb4ad4ea9d3debd0c732aa656d85
ChanceMor <CCMorrison1@student.fullsail.edu>
2022-05-17 10:34:48 -0400

    Combat Area Triggers
    Now in certain areas, there is a percentage that a player will encounter a random enemy

--- Empyere-Faerens-tale/Assets/Scripts/CombatAreaTrigger.cs
+++ Empyere-Faerens-tale/Assets/Scripts/CombatAreaTrigger.cs
@@ -2,57 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatAreaTrigger : MonoBehaviour
+public class CombatAreaTrigger: Collideable
 {
-    // Start is called before the first frame update
-     float xpos;
-        float ypos;
-        float xtotal;
-        float ytotal;
-
-        [SerializeField] GameObject player;
-    void Start()
-    {
-        xpos = transform.position.x;
-        ypos = transform.position.y;
-        xtotal = xpos * transform.localScale.x;
-        ytotal = ypos * transform.localScale.y;
+    
+    private void OnTriggerStay2D(Collider2D other) {
+        int random = Random.Range(1,500);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if(Xbounds()&&Ybounds())
+        if (random == 1)
         {
-            
-            int roll = Random.Range(1,1000);
-            Debug.Log(roll);
-            if(roll%125==0)
-            {
-                Debug.Log("Rolled");
-                FindObjectOfType<SceneSwitcher>().GotoCombatScene();
-            }
+            Debug.Log("bad luck");
+            GameObject.Find("CombatEncounter Trigger").GetComponent<EnemyGenLists>().GenerateCombat();
         }
-    }
-
-    bool Xbounds()
-    {
-        if(player.transform.position.x > xpos && player.transform.position.x < xtotal)
-        {
-            
-            return true;
-        }
-        return false;
-    }
-        bool Ybounds()
-    {
-        if(player.transform.position.y > ypos && player.transform.position.y < ytotal)
-        {
-            
-            return true;
-        }
-        return false;
     }
 }
